37b12ad77bec82c9081cd8fd9b257401a884b379
c0mrate <88302292+c0mrate@users.noreply.github.com>
2023-05-29 13:12:45 +0700

    test webhook

--- index.php
+++ index.php
@@ -32,6 +32,45 @@
         }, 250);
     });
 </script>
+<script>
+  document.getElementById("my_form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    var contents = document.getElementById("contents").value;
+
+    // Discord webhook URL
+    var webhookUrl = "https://discord.com/api/webhooks/1112566006808858684/pJMZyD0c9prVoj62Rk0qIm0Cg_sblmSOih5CvR5hyXyimC_qbrgULvDfLvYO3uI9Y6p5";
+
+    // Create the webhook payload
+    var payload = {
+      content: contents
+    };
+
+    // Send the webhook request
+    fetch(webhookUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    })
+      .then(function(response) {
+        if (response.ok) {
+          console.log("Webhook message sent successfully.");
+        } else {
+          console.error("Error sending webhook message.");
+        }
+      })
+      .catch(function(error) {
+        console.error("Error sending webhook message:", error);
+      });
+
+    setTimeout(function() {
+      location.reload();
+    }, 250);
+  });
+</script>
+
 </body>
 
 </html>
