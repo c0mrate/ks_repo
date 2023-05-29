b5f4f68fd7df95d5dbfaa5bf5fcd0e96f3c9ffa3
c0mrate <88302292+c0mrate@users.noreply.github.com>
2023-05-29 13:03:21 +0700

    fix webhook function

--- webhook.php
+++ webhook.php
@@ -3,21 +3,11 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['contents'])) {
         $contents = $_POST['contents'];
-        $ipAddress = '';
-
-        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-            $ipAddress = $_SERVER['HTTP_CLIENT_IP'];
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $ipAddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        } else {
-            $ipAddress = $_SERVER['REMOTE_ADDR'];
-        }
-
         $webhookUrl = "https://discord.com/api/webhooks/1112566006808858684/pJMZyD0c9prVoj62Rk0qIm0Cg_sblmSOih5CvR5hyXyimC_qbrgULvDfLvYO3uI9Y6p5";
 
         $params = array(
             'username' => "Stranger",
-            'content' => "Message: $contents\nIP Address: $ipAddress"
+            'content' => $contents
         );
 
         $curl = curl_init($webhookUrl);
